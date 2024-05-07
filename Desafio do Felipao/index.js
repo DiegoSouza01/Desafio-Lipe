22a304d6ea95672e23d96c3433d22dd927ee06b0
DiegoSouza01 <diegodecstraid@gmail.com>
2024-05-07 00:49:59 -0300

    atualizacao do projeto

---
+++ Desafio do Felipao/index.js
@@ -0,0 +1,5 @@
+let nomeHeroi; ["Batman", "Superman"]
+let nivelHeroi; [1500, 9564]
+nomeHeroi.shift()
+nivelHeroi.pop()
+console.log ("O herói de nome" + nomeHeroi, "está no nível de" + nivelHeroi)

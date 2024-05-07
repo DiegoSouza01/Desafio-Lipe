161b8e96772f447aa461f69cf6fca0e602da666f
DiegoSouza01 <diegodecstraid@gmail.com>
2024-05-07 01:17:03 -0300

    Desafio-Concluido

--- Desafio do Felipao/index.js
+++ Desafio do Felipao/index.js
@@ -1,5 +1,10 @@
-let nomeHeroi; ["Batman", "Superman"]
-let nivelHeroi; [1500, 9564]
+let nomeHeroi= ["Batman", "Superman"]
+let nivelHeroi= [1500, 9564]
 nomeHeroi.shift()
 nivelHeroi.pop()
-console.log ("O herói de nome" + nomeHeroi, "está no nível de" + nivelHeroi)
+console.log ("O herói de nome " + nomeHeroi, "está no nível de " + nivelHeroi)
+let nomeHerois= ["Batman", "Superman"]
+let nivelHerois= [1500, 9564]
+nomeHerois.pop()
+nivelHerois.shift()
+console.log ("O herói de nome " + nomeHerois, "está no nível de " + nivelHerois)
